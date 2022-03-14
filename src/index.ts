2cbd595c14caa9abdb7935f988fa445934896413
tupe12334 <ofek.gabay.he@gmail.com>
2022-03-14 15:55:37 +0200

    fix: update index page

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-import merge from 'lodash/merge';
-import buildDataProvider from 'ra-data-graphql';
-import pluralize from 'pluralize';
 import { camelCase } from 'camel-case';
-
-import defaultBuildQuery from './buildQuery';
-
+import merge from 'lodash/merge';
+import pluralize from 'pluralize';
 import {
+  DataProvider,
   GET_LIST,
-  GET_ONE,
   GET_MANY,
   GET_MANY_REFERENCE,
-  DELETE_MANY,
-  UPDATE_MANY,
+  GET_ONE,
 } from 'ra-core';
-
-import { IntrospectionObjectType, FetchType } from './types';
+import buildDataProvider, { BuildQueryFactory, Options } from 'ra-data-graphql';
+import defaultBuildQuery from './buildQuery';
+import { IntrospectionObjectType } from './types';
 
 const defaultOptions = {
   buildQuery: defaultBuildQuery,
@@ -33,24 +29,19 @@ const defaultOptions = {
   },
 };
 
-export const buildQuery = defaultBuildQuery;
-
-export default (options: any) => {
+export default (
+  options: Omit<Options, 'buildQuery'> & { buildQuery?: BuildQueryFactory }
+): Promise<DataProvider> => {
   return buildDataProvider(merge({}, defaultOptions, options)).then(
-    (defaultDataProvider: any) => {
-      return (
-        fetchType: FetchType,
-        resource: IntrospectionObjectType,
-        params: any
-      ) => {
-        if (fetchType === DELETE_MANY) {
+    (defaultDataProvider) => {
+      return {
+        ...defaultDataProvider,
+        deleteMany: () => {
           throw new Error(`DELETE_MANY is not supported by this data provider`);
-        }
-        if (fetchType === UPDATE_MANY) {
+        },
+        updateMany: () => {
           throw new Error(`UPDATE_MANY is not supported by this data provider`);
-        }
-
-        return defaultDataProvider(fetchType, resource, params);
+        },
       };
     }
   );
